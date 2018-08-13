b619995d2bfe2400aa159e8006be02ccc7df1ec6
shadowman <agascompany@gmail.com>
2018-08-13 10:18:29 +0300

    requesting location services

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import MapKit
+import  CoreLocation
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regioRadius: Double = 1000
+    
+    @IBAction func centerMapBtnWasPrsd(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +39,24 @@ class MapVC: UIViewController {
 
 }
 
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else {return}
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regioRadius * 2, regioRadius * 2)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate{
+    func configureLocationServices(){
+        if authorizationStatus == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        }else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+}
